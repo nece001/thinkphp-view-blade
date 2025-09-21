5304b13bd14431bffdc4b60973c4e82ad5d1d387
nece001 <nece001@163.com>
2025-09-21 14:32:47 +0800

    添加判断

--- src/config/blade.php
+++ src/config/blade.php
@@ -3,6 +3,11 @@
 // 由于Blade模板引擎依赖的env()函数，早于thinkphp的助手文件被加载，导致thinkphp的env()函数失效。
 // 因此先初始Blade的env()函数可以读取.env文件，替代thinkphp的env()函数。
 // 目前只发现evn()函数重名。
-$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
+try {
+    $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
+} catch (\Exception $e) {
+    echo PHP_EOL, 'Blade模板引擎不存在：', $e->getMessage(), PHP_EOL;
+    echo '请手动删除配置文件：' . __FILE__, PHP_EOL;
+}
 return [];
